cd3b14a2a1d6d9c23c75e249d1ffbd42a817ab8c
shinwoos <kmnj2100@gmail.com>
2025-01-02 15:13:48 +0900

    test code

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FirstTest {
+
+    @Test
+    void t1(){
+        int rst = 1;
+        assertThat(rst).isEqualTo(1);
+    }
+}
