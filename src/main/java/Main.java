d57597dfd58d5cb977eb0f3814b901a9d2d38e92
shinwoos <kmnj2100@gmail.com>
2025-01-02 14:50:43 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner("aaa\nbbb\n");
+
+        String val1 = sc.nextLine();
+        System.out.println(val1);
+
+        String val2 = sc.nextLine();
+        System.out.println(val2);
+
+    }
+}
