f2922de71d2c4542d8121f5904c6aeeda61d4222
shinwoos <kmnj2100@gmail.com>
2025-01-03 12:14:18 +0900

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,28 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        test2();
+    }
 
+    public static void test2() {
+        // System.out -> 표준출력 -> 모니터(콘솔)
+
+        // 2. 출력문의 값을 문자열로 받아서 봇에게 줄 수 있다.
+        PrintStream origin = System.out;
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        System.out.println("hello");
+
+        System.setOut(origin);
+        String str = out.toString();
+        System.out.println(str);
+    }
+
+    public static void test1() {
         Scanner sc = new Scanner("aaa\nbbb\n");
 
         String val1 = sc.nextLine();
@@ -11,5 +31,8 @@ public class Main {
         String val2 = sc.nextLine();
         System.out.println(val2);
 
+        //test bot 만들기
+        TestApp app = new TestApp();
+        app.run();
     }
 }
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,9 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,5 +13,44 @@ public class FirstTest {
     void t1(){
         int rst = 1;
         assertThat(rst).isEqualTo(1);
+    }
+
+    @Test
+    void t2(){
+        TestApp app = new TestApp();
+        app.run();
+
+     //   assertThat(result).isEqualTo("aaa");
+    }
+
+    @Test
+    void t3(){
+        //테스트 봇 선입력
+        Scanner sc = new Scanner("종료\n");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        TestApp app = new TestApp();
+        app.run();
+
+        assertThat(out.toString()).contains("명언앱 종료");
+    }
+
+    @Test
+    @DisplayName("== 명언 앱 == 출력")
+    void t4(){
+        //테스트 봇 선입력
+        Scanner sc = new Scanner("종료\n");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        TestApp app = new TestApp();
+        app.run();
+
+        assertThat(out.toString())
+                .contains("== 명언 앱 ==")
+                .contains("명언앱 종료");
     }
 }
