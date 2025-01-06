33fd9f2726e8c795e36ed2b4f809af56a9e30537
shinwoos <kmnj2100@gmail.com>
2025-01-06 10:25:02 +0900

    command test

--- src/main/java/app/global/Command.java
+++ src/main/java/app/global/Command.java
@@ -2,11 +2,19 @@ package app.global;
 
 public class Command {
 
+    public Command(){
+
+    };
+    public Command(String cmd){
+
+    };
+
+    public String getActionName() {
+        return "삭제";
+    };
     //쪼개기 작업
 
     //회원
-
-    //로그인?id=aaa&pw=www
 
 
 }
